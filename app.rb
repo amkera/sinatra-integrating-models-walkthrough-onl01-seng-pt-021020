d375545f7e3bb379629399ec3183574a38459526
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-05-06 22:52:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,10 +5,12 @@ class App < Sinatra::Base
   get '/' do
     erb :index
   end
+  #This renfers the index.reb page
 
   post '/' do
     text_from_user = params[:user_text]
 
     erb :results
   end
+  #This receives the form data through the params hash, and renders the results page. 
 end
