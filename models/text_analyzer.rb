ea31663e6a3263263910b2ef4e1d6ac8dd83658c
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-05-06 22:53:30 +0000

    Automatically backed up by Learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,2 +1,3 @@
 # Your TextAnalyzer model code will go here.
-#
+# We could analyze all of the data from params[:user_text] in our application controller, but our route would get messy very quickly. Instead, let's create a new class inside of our models directory that will take care of the analysis of the text. In your models directory, open the file called text_analyzer.rb.
+
