bd5c626a8d674e1ba1e1f6407877eb67187c0951
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-05-06 22:58:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,16 +1,24 @@
 require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
+#This now gives us the ability to reference the TextAnalyzer model / class and invoke its .new method.
 
 class App < Sinatra::Base
   get '/' do
     erb :index
   end
-  #This renfers the index.reb page
+  #This renders the index.reb page
+
+
 
   post '/' do
     text_from_user = params[:user_text]
-
+    #this needs to be fed into a new instance of the Text Analyzer class. 
+    @analyzed_text = TextAnalyzer.new(text_from_user)
     erb :results
   end
+  
+  
+  
+  
   #This receives the form data through the params hash, and renders the results page. 
 end
